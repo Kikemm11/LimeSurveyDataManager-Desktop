8142d1807f8746a0056dcbb5ca871be409f411b9
Kikemm11 <Kikemaldonado11@gmail.com>
2024-08-14 14:30:21 -0400

    Update the amount of buttons in StartWindow and change its logic to act within the content of a directory selected by the user including all the file validations

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-import sys
-import tkinter as tk
-
 from windows.StartWindow import StartWindow
 from windows.ProcessWindow import ProcessWindow
 from windows.EndWindow import EndWindow
@@ -8,6 +5,6 @@ from windows.EndWindow import EndWindow
 if __name__ == "__main__":
     
     start_window = StartWindow()
-    process_window = ProcessWindow(start_window.survey_dict, start_window.exported_files, start_window.survey_id)
+    process_window = ProcessWindow(start_window.survey_dict, start_window.csv_files, start_window.survey_id)
     end_window = EndWindow(process_window.output_df)
     
--- windows/StartWindow.py
+++ windows/StartWindow.py
@@ -1,10 +1,11 @@
+import os
+import csv
+
 import tkinter as tk
 from tkinter import filedialog
 
 import settings
-from windows.ProcessWindow import ProcessWindow
 
-import re
 
 class StartWindow(tk.Tk):
     
@@ -15,15 +16,15 @@ class StartWindow(tk.Tk):
         self.configure(bg=settings.BACKGROUND_COLOR)
         self.resizable(False, False)
         
-        for i in range(6):  
+        for i in range(7):  
             self.grid_rowconfigure(i, weight=1)
 
         for j in range(3):  
             self.grid_columnconfigure(j, weight=1)
         
         self.survey_dict = {}
-        self.lime_survey_file = []
-        self.exported_files = []
+        self.txt_files = None
+        self.csv_files = None
         self.survey_id = None
         
         
@@ -48,9 +49,9 @@ class StartWindow(tk.Tk):
         main_text.grid(row=1, column=1, sticky="nsew")
         
         
-        lime_survey_button = tk.Button(self, 
-                        text="Select LimeSurvey file", 
-                        command=self.select_survey_file,
+        directory_button = tk.Button(self, 
+                        text="Select Directory", 
+                        command=self.select_directory,
                         font=(settings.FONT, 14),
                         bg=settings.BACKGROUND_BUTTON_COLOR,  
                         fg='black',    
@@ -58,22 +59,9 @@ class StartWindow(tk.Tk):
                         pady=5
                     )
         
-        lime_survey_button.grid(row=2, column=1, sticky="nsew", padx=170, pady=20)
+        directory_button.grid(row=3, column=1, sticky="nsew", padx=170, pady=20)
 
 
-        responses_button = tk.Button(self, 
-                        text="Select the DBResults files", 
-                        command=self.select_exported_files,
-                        font=(settings.FONT, 14),
-                        bg=settings.BACKGROUND_BUTTON_COLOR,  
-                        fg='black',    
-                        padx=10,       
-                        pady=5
-                    )
-
-        responses_button.grid(row=3, column=1, sticky="nsew", padx=170, pady=20)
-        
-        
         process_button = tk.Button(self, 
                         text="Process", 
                         command=self._change_window,
@@ -84,50 +72,75 @@ class StartWindow(tk.Tk):
                         pady=5
                     )
 
-        process_button.grid(row=5, column=1, sticky="nsew", padx=250, pady=20)
+        process_button.grid(row=6, column=1, sticky="nsew", padx=250, pady=20)
         
         self.mainloop()
+
+
+
+    def select_directory(self):
+        selected_directory = filedialog.askdirectory(title="Select a Directory")
+        if selected_directory:
+            self.txt_files = [os.path.join(selected_directory, file) for file in os.listdir(selected_directory) if os.path.join(selected_directory, file).endswith('.txt')]
+            self.survey_id = [''.join(filter(str.isdigit, file.split('_')[2])) for file in os.listdir(selected_directory) if os.path.join(selected_directory, file).endswith('.txt')]
+            self.csv_files = [os.path.join(selected_directory, file) for file in os.listdir(selected_directory) if os.path.join(selected_directory,file).endswith('.csv')]
+            
     
-    
-    def _change_window(self): 
-        if self.lime_survey_file and len(self.exported_files) > 0:
-            self.destroy()
+    def validate_survey(self):
+        if self.txt_files:
+            if len(self.txt_files) > 1:
+                settings.show_error_message("It looks like you've got more than one limesurvey.txt file inside your directory")
+                exit()
+            else:
+                with open(self.txt_files[0], 'r') as survey_file:
+                    if survey_file.readline() in 'id,related_id,class,typscale,name,relevance,text,help,language,validation,mandatory,encrypted,other,default,same_default,same_script,allowed_filetypes,alphasort,answer_order,answer_width,answer_width_bycolumn,array_filter,array_filter_exclude,array_filter_style,assessment_value,category_separator,choice_input_columns,choice_title,commented_checkbox,commented_checkbox_auto,cssclass,date_format,date_max,date_min,display_columns,display_rows,display_type,dropdown_dates,dropdown_dates_minute_step,dropdown_dates_month_style,dropdown_prefix,dropdown_prepostfix,dropdown_separators,dropdown_size,dualscale_headerA,dualscale_headerB,em_validation_q,em_validation_q_tip,em_validation_sq,em_validation_sq_tip,equals_num_value,equation,exclude_all_others,exclude_all_others_auto,hidden,hide_tip,input_boxes,input_size,label_input_columns,location_city,location_country,location_defaultcoordinates,location_mapheight,location_mapservice,location_mapwidth,location_mapzoom,location_nodefaultfromip,location_postal,location_state,max_answers,max_filesize,max_num_of_files,max_num_value,max_num_value_n,max_subquestions,maximum_chars,min_answers,min_num_of_files,min_num_value,min_num_value_n,multiflexible_checkbox,multiflexible_max,multiflexible_min,multiflexible_step,num_value_int_only,numbers_only,other_comment_mandatory,other_numbers_only,other_replace_text,page_break,parent_order,placeholder,prefix,printable_help,public_statistics,random_group,random_order,rank_title,repeat_headings,reverse,samechoiceheight,samelistheight,scale_export,show_comment,show_grand_total,show_title,show_totals,showpopups,slider_accuracy,slider_custom_handle,slider_default,slider_default_set,slider_handle,slider_layout,slider_max,slider_middlestart,slider_min,slider_orientation,slider_rating,slider_reset,slider_reversed,slider_separator,slider_showminmax,statistics_graphtype,statistics_showgraph,statistics_showmap,suffix,text_input_columns,text_input_width,time_limit,time_limit_action,time_limit_countdown_message,time_limit_disable_next,time_limit_disable_prev,time_limit_message,time_limit_message_delay,time_limit_message_style,time_limit_timer_style,time_limit_warning,time_limit_warning_2,time_limit_warning_2_display_time,time_limit_warning_2_message,time_limit_warning_2_style,time_limit_warning_display_time,time_limit_warning_message,time_limit_warning_style,use_dropdown,value_range_allows_missing':
+                        settings.show_error_message("Incorrect limesurvey file")
+                        exit()
+                    else:
+                        self.survey_id = self.survey_id[0]
+                        self._get_survey_dict(survey_file.readlines())
+                        return True
         else:
-            settings.show_error_message("You've got to select one or more BDResult.csv files")
-                
-    def _get_survey_dict(self):
+            settings.show_error_message("It looks like you've got no limesurvey.txt file inside your directory. Please provide one")
+            exit()
+
+
+    def validate_csv_files(self):
+
+        if len(self.csv_files) > 0:
+
+            for file in self.csv_files:
+
+                with open(file, newline='') as csvfile:
+                    reader = csv.reader(csvfile)
+                    for row in reader:
+                        for i in range(7, len(row)):
+                            if 'filecount' not in row[i]:
+                                code = row[i].split('X')
+                                if  not (code[0] == self.survey_id and self.survey_dict.get(code[1]) and self.survey_dict.get(code[2])):
+                                    settings.show_error_message("Ups, one of your DBResults file does not belong to the same survey you're trying to process")
+                                    exit()
+                        break
+                    
+                    
+            return True
         
-        for line in self.lime_survey_file:
+        else:
+            settings.show_error_message("Ups, you've got no DBResults files in this directory")
+            exit() 
+        
+                
+    def _get_survey_dict(self, limesurvey_file):
+        
+        for line in limesurvey_file:
             elements = line.split(',')
             if not self.survey_dict.get(elements[0]):
                  self.survey_dict[elements[0]] = {'Q': elements[6].rstrip(), 'A': {}}
             else:
                 self.survey_dict[elements[0]]['A'][elements[4]] = elements[6].rstrip()
     
-    def select_survey_file(self):
-        file_path = filedialog.askopenfilename(
-            title="Select a File",
-            filetypes=[("Text Files", "*.txt")]
-        )
 
-        lime_survey_pattern = re.compile(r'limesurvey_survey_[0-9]+')
-        
-        if file_path: 
-            if 'limesurvey_survey' in file_path:
-                self.survey_id = ''.join(filter(str.isdigit, file_path.split('_')[2]))
-                with open(file_path, 'r') as file:
-                    self.lime_survey_file = file.readlines()
-                    self._get_survey_dict()
-            else: 
-                settings.show_error_message("The file must be an exported .txt file from LimeSurvey!")
-        else:
-            settings.show_error_message("You've got to select one limesurvey.txt file")
-     
-        
-    def select_exported_files(self):
-        
-        self.exported_files = filedialog.askopenfilenames(
-            title="Select Files",
-            filetypes=[("CSV Files", "*.csv")]
-        )
-        
+
+    def _change_window(self): 
+        if self.validate_survey() and self.validate_csv_files():
+            self.destroy()
