7dbc2fd260767c3142bcb01e0246737e55af3352
Kikemm11 <Kikemaldonado11@gmail.com>
2024-08-16 09:31:18 -0400

    Update the html_template to render an icon which leads to GoogleMaps given the latitutde and longitude provided in the table and fix some style details

--- views/output_html.py
+++ views/output_html.py
@@ -5,10 +5,30 @@ html_template = """
     <title>Survey Responses</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    
+    <style>
+        table {
+            table-layout: auto; 
+            width: 100%; 
+        }
+        th, td {
+            text-align: center; 
+            white-space: nowrap; 
+            padding: 8px; 
+        }
+        th {
+            white-space: nowrap; 
+            font-weight: bold; 
+        }
+        td {
+            white-space: normal; 
+        }
+    </style>
+  
 </head>
 <body>
     <div class="container">
-        <h1>Survey Responses</h1>
+         <h1 class="text-center mb-5 mt-4">Survey Responses</h1>
 
         <div class="table-container">
             <table class="table mt-3">
@@ -23,14 +43,21 @@ html_template = """
                     {% for i in range(df_lenght) %}
                         <tr>
                             {% for col in column_names %}    
-                                {% if 'Foto' not in col %}
+                                {% if ('Foto' not in col) and ('Ubicación' not in col) %}
                                     <td>{{ output_df.iloc[i][col] }}</td>
                                 {% else %}
-                                    {% if img_dict.get(output_df.iloc[i][col]) %}
-                                        <td><img src="{{ img_dict.get(output_df.iloc[i][col]) }}"  width="100" height="100"></td>
+                                    {% if 'Foto' in col %}
+                                        {% if img_dict.get(output_df.iloc[i][col]) %}
+                                            <td><img src="{{ img_dict.get(output_df.iloc[i][col]) }}"  width="100" height="100"></td>
+                                        {% else %}
+                                            <td>{{ output_df.iloc[i][col] }}</td>
+                                        {% endif %}
                                     {% else %}
-                                        <td>{{ output_df.iloc[i][col] }}</td>
-                                    {% endif %}
+                                        <td>
+                                            <a href="https://www.google.com/maps?q= {{ output_df.iloc[i][col] }}" target="_blank">
+                                            <img src="https://img.icons8.com/ios-filled/50/000000/map-marker.png" alt="Map Icon" width="30" height="30">
+                                        </td>    
+                                    {% endif %}    
                                 {% endif %}
                             {% endfor %}
                         </tr>
